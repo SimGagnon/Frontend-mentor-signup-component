6364618fe5075e91a0190a825417a03fb3a7f712
Simon Gagnon <gagnonbsimon@gmail.com>
2021-08-22 20:59:39 -0400

    added form validation

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,23 @@
-console.log("test");
+
+const email = document.getElementById("emailInput");
+const theInputs = document.querySelectorAll("input");
+
+form.addEventListener('submit', (e) => {
+    const emailValue = email.value;
+    e.preventDefault();
+
+    for (let i = 0; i < theInputs.length; i++) {
+        !theInputs[i].value ? theInputs[i].parentElement.classList.add("error") : theInputs[i].parentElement.classList.remove("error");
+    }
+
+    if (!validateEmail(emailValue)) {
+        email.parentElement.classList.add('error');
+    } else {
+        email.classList.remove('error');
+    }
+});
+
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
