d94c83b01b22469dcbcd7f89542acb2f7478c0a9
Simon Gagnon <gagnonbsimon@gmail.com>
2021-08-22 21:54:13 -0400

    cleaned javascript

--- app/js/script.js
+++ app/js/script.js
@@ -1,23 +1,26 @@
-
 const email = document.getElementById("emailInput");
 const theInputs = document.querySelectorAll("input");
+const form = document.querySelector("form");
 
 form.addEventListener('submit', (e) => {
     const emailValue = email.value;
     e.preventDefault();
 
-    for (let i = 0; i < theInputs.length; i++) {
-        !theInputs[i].value ? theInputs[i].parentElement.classList.add("error") : theInputs[i].parentElement.classList.remove("error");
-    }
+    theInputs.forEach(input => {
+        !input.value ? input.parentElement.classList.add("error") : input.parentElement.classList.remove("error");
+    });
 
-    if (!validateEmail(emailValue)) {
-        email.parentElement.classList.add('error');
-    } else {
-        email.classList.remove('error');
-    }
+    !validateEmail(emailValue) ? email.parentElement.classList.add('error') : email.classList.remove('error');
 });
 
 function validateEmail(email) {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
 }
+
+
+theInputs.forEach(input => {
+    input.addEventListener('input', () => {
+        input.value ? input.parentElement.classList.remove("error") : null;
+    })
+})
